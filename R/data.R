2854df7147a5b11fb220733cbd9110ae4217b7ad
Victor Lee <112950796+vicyklee@users.noreply.github.com>
2025-10-17 17:54:47 +0200

    Create data.R

---
+++ R/data.R
@@ -0,0 +1,61 @@
+#' "Jain" dataset
+#'
+#' @format ## `jain`
+#' A data frame with 373 rows and 4 columns:
+#' \describe{
+#'   \item{x, y}{Spatial coordinates of the original dataset}
+#'   \item{age}{Randomly sampled age}
+#'   \item{clust}{True spatial cluster}
+#' }
+#' @source <https://cs.joensuu.fi/sipu/datasets/>
+#' @references
+#' Jain, A. K., & Law, M. H. (2005, December). Data clustering: A user’s dilemma. In International conference on pattern recognition and machine intelligence (pp. 1-10). Berlin, Heidelberg: Springer Berlin Heidelberg.
+#' 
+#' Fränti, P., & Sieranoja, S. (2018). K-means properties on six clustering benchmark datasets. Applied intelligence, 48(12), 4743-4759.
+"jain"
+
+#' "Asymmetric" dataset
+#'
+#' @format ## `asymmetric`
+#' A data frame with 1000 rows and 4 columns:
+#' \describe{
+#'   \item{x, y}{Spatial coordinates of the original dataset}
+#'   \item{age}{Randomly sampled age}
+#'   \item{clust}{True spatial cluster}
+#' }
+#' @source <https://cs.joensuu.fi/sipu/datasets/>
+#' @references
+#' Fränti, P., & Sieranoja, S. (2018). K-means properties on six clustering benchmark datasets. Applied intelligence, 48(12), 4743-4759.
+#' 
+#' Rezaei, M., & Fränti, P. (2020). Can the number of clusters be determined by external indices?. IEEE Access, 8, 89239-89257.
+"asymmetric"
+
+#' "Aggregation" dataset
+#'
+#' @format ## `aggregation`
+#' A data frame with 788 rows and 4 columns:
+#' \describe{
+#'   \item{x, y}{Spatial coordinates of the original dataset}
+#'   \item{age}{Randomly sampled age}
+#'   \item{clust}{True spatial cluster}
+#' }
+#' @source <https://cs.joensuu.fi/sipu/datasets/>
+#' @references
+#' Gionis, A., Mannila, H., & Tsaparas, P. (2007). Clustering aggregation. Acm transactions on knowledge discovery from data (tkdd), 1(1), 4-es.
+#' 
+#' Rezaei, M., & Fränti, P. (2020). Can the number of clusters be determined by external indices?. IEEE Access, 8, 89239-89257.
+"aggregation"
+
+
+#' "Noisycircles" dataset
+#'
+#' @format ## `noisycircles`
+#' A data frame with 1000 rows and 4 columns:
+#' \describe{
+#'   \item{x, y}{Spatial coordinates of the original dataset}
+#'   \item{age}{Randomly sampled age}
+#'   \item{clust}{True spatial cluster}
+#' }
+#' @source [factoextra::multishapes]
+"noisycircles"
+
