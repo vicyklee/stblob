e3c939961a1fb29081ee81fb1c8f2e86a427041a
Victor Lee <112950796+vicyklee@users.noreply.github.com>
2026-01-18 17:22:57 +0100

    Update mesoneo.R

--- paper/scripts/mesoneo.R
+++ paper/scripts/mesoneo.R
@@ -14,37 +14,18 @@ plan(multisession)
 ggplot2::theme_set(ggplot2::theme_bw())
 
 #------------------------------------------------------------------------------#
-# Archaeological (aDNA) dataset 
+# Archaeological (aDNA) dataset N=1207 k=c(5,12) ####
 #------------------------------------------------------------------------------#
 data <- readr::read_tsv(here("paper/data/mesoneo.tsv")) %>%
   filter(stringr::str_detect(Region, "Europe") | Region == "WesternAsia",
          Country != "Faroes" & Country != "Greenland" & Country != "Iceland",
          Age_average <= 15000)
 
-set.seed(123)
-data <- data[sample(1:nrow(data), 300),]
-
-# p_s <- plot_space(data, clust = data$Region, coords = c("Longitude", "Latitude"), alpha = 0.5) +
-#   labs(colour = "region") +
-#   scale_colour_viridis_d(option = 15) 
-# p_t <- plot_time(data, clust = data$Region, age = "Age_average") +
-#   theme(legend.position = "none") +
-#   xlab("time (ybp)") +
-#   labs(colour = "region") +
-#   scale_colour_viridis_d(option = 15) +
-#   scale_fill_viridis_d(option = 15) +
-#   scale_x_reverse()
-
-# p_mesoneo <- wrap_plots(p_s, p_t, nrow=1, guides = "collect")
-# save_pdf(p_mesoneo, here("paper/figures/mesoneo300.pdf"), width = 10, height = 4)
-
-# blob_moo
 start <- Sys.time()
 set.seed(123)
-res <- blob_moo(data, k = c(5,6), r = c(0.9,1), run = 20, batch = 30,
-                coords = c("Longitude","Latitude"), age = "Age_average",
-                pareto_similar_ari = 0.8, obj = c("n_removed", "space_wcd", "-time_wcr", "-time_wce"))
+res <- stblob(data, k = c(5,6), r = c(0.95,1), run = 50, batch = 200,
+              coords = c("Longitude","Latitude"), age = "Age_average",
+              pareto_similar_ari = 0.8, obj = c("n_removed", "space_wcd", "-time_wcr", "-time_wce"))
 end <- Sys.time()
 print(end-start)
-saveRDS(res,file = here("paper/output/mesoneo300_res_k5to6.rds"))
-# res <- readRDS( here("paper/output/mesoneo300_res_k5to6.rds"))
+saveRDS(res,file = here("paper/output/mesoneo_res_k5to12.rds"))
